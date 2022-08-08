0c40ee297bb905941dcbd0f9c42f17fdd4466932
Jakub.Lerl <Jakub.Lerl@absa.africa>
2022-08-08 12:02:41 +0200

    add uvaly august

--- scripts/export_body.py
+++ scripts/export_body.py
@@ -14,6 +14,7 @@ logger.setLevel('INFO')
 csv_url = 'https://docs.google.com/spreadsheets/d/1IjrWe_b-Wk5n9hD2CJl5yiS_BoN9wU203jpGx4qfP6Q/export?format=csv'
 headers_count = 2
 
+
 class Player:
     def __init__(self, name: str):
         self.name = name
@@ -24,6 +25,7 @@ class Score:
         self.order = int(order)
         self.name = name.strip()
         self.points = int(points)
+
 
 def export_bodu(file_path: str):
     logger.info(f'pulling data from {csv_url}')
@@ -57,18 +59,26 @@ def export_bodu(file_path: str):
             score = Score(order, name, points)
             scores[name] = score
     
-    logger.info(f'dumping data')
-    for player in players:
-        if player.name in scores:
-            score = scores[player.name]
-            print(f"{score.name};{score.points}")
-            scores.pop(player.name)
-        else:
-            print(f"{player.name}; ")
-    
+    missing_players = []
+    source_players = list(map(lambda p: p.name, players))
     for player_name in scores:
-        score = scores[player_name]
-        print(f"{score.name};{score.points}")
+        if player_name not in source_players:
+            missing_players.append(player_name)
+
+    if missing_players:
+         print('--- players missing in source sheet ---')
+         for player_name in missing_players:
+             print(player_name)
+    else:
+        logger.info(f'paste this to source sheet score column:')
+        for player in players:
+            if player.name in scores:
+                score = scores[player.name]
+                print(f"{score.name};{score.points}")
+                scores.pop(player.name)
+            else:
+                print(f" ")
+
 
 if __name__ == '__main__':
     args = sys.argv[1:]
